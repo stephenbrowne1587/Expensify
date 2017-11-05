652ac88229b85dec875b57e45958f62d89f3a402
Stephen Browne <stephenbrowne1587@gmail.com>
2017-11-05 16:25:44 -0500

    Setup production webpack build

--- src/app.js
+++ src/app.js
@@ -10,39 +10,12 @@ import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 import "react-dates/lib/css/_datepicker.css"
 
-
 const store = configureStore();
-
-// const expenseOne = store.dispatch(addExpense({
-//   description: "Water bill",
-//   note: "This is the water bill",
-//   amount: 4500,
-//   }
-// ));
-
-// const expenseTwo = store.dispatch(addExpense({
-//   description: "Gas bill",
-//   note: "This is the gas bill",
-//   createdAt: 1000
-//   }
-// ));
-// const expenseThree = store.dispatch(addExpense({
-//   description: "Rent",
-//   note: "This is the rent bill",
-//   amount: 109500,
-//   }
-// ));
-
-
-// const state = store.getState();
-// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-// console.log(visibleExpenses);
 
 const jsx = (
   <Provider store={store}>
     <AppRouter />
   </Provider>
-  
 );
 
 ReactDOM.render(jsx, document.getElementById("app"));
