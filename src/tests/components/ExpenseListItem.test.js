e494265d20c9d4bae3176e3e41cdc3148b796982
Stephen Browne <stephenbrowne1587@gmail.com>
2017-11-05 21:06:33 -0500

    setup formatting for ammount

--- src/tests/components/ExpenseListItem.test.js
+++ src/tests/components/ExpenseListItem.test.js
@@ -3,7 +3,6 @@ import { shallow } from "enzyme";
 import ExpenseListItem  from "../../components/ExpenseListItem";
 import expenses from "../fixtures/expenses";
 
-
 test("should render expense list item", () => {
   const wrapper = shallow(<ExpenseListItem {...expenses[0]}/>);
   expect(wrapper).toMatchSnapshot();
