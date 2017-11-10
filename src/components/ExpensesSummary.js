d9cba19384c2f0efa792edbbd67dfcb79e2c8933
Stephen Browne <stephenbrowne1587@gmail.com>
2017-11-10 12:40:33 -0500

    style login page and heard

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {connect} from "react-redux";
+import {Link} from "react-router-dom";
 import selectExpenses from "../selectors/expenses";
 import getExpenseTotal from "../selectors/expenses-total";
 import numeral from "numeral";
@@ -8,11 +9,18 @@ export const ExpenseSummary = ({expenseCount, expensesTotal}) => {
   const expenseWord = expenseCount === 1 ? "expense" : "expenses";
   const formattedTotal = numeral(expensesTotal/100).format("$0,0.00");
   return (
-    <div>
-      <h1>
-        Viewing {expenseCount} {expenseWord} totalling {formattedTotal}
-      </h1>
-
+    <div className="page-header">
+      <div className="content-container">
+        <h1 className="page-header__title">
+          Viewing 
+          <span> {expenseCount} </span>
+           {expenseWord} totalling 
+           <span> {formattedTotal} </span>
+        </h1>
+        <div className="page-header__actions">
+          <Link className="button" to="/create">Add Expense</Link>
+        </div>
+      </div>
   </div>
   )
 }
