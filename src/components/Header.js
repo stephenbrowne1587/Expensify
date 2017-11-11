696133bd910282f09e933f4c9a18fdf52e51c48d
Stephen Browne <stephenbrowne1587@gmail.com>
2017-11-11 12:37:57 -0500

    finish styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,9 +10,10 @@ export const Header = ({startLogout}) => (
       <Link className="header__title" to="/dashboard" >
         <h1>Expensify</h1>
       </Link>
-      <button className="button button--link" onClick={startLogout}>Logout</button>
+      <button className="button button--link" onClick={startLogout}>
+        Logout
+      </button>
     </div>
-    
   </div>
     
   </header>
